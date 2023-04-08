fca4e50fbcbf4379c755b2e4e6c2cab092bb7c72
ravinamore03 <129398915+ravinamore03@users.noreply.github.com>
2023-04-09 00:36:39 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default {
-  base: "/cardapp-react/",
-};
+export default defineConfig({
+  plugins: [react()],
+})
